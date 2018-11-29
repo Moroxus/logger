a95c51018438fc1e91862da07beca41e0cea17f9
Lukáš Tomovič <lukas.tomovic@gmail.com>
2018-11-29 09:18:40 +0100

    Add some doxygen comments to some static methods.

--- include/logger.h
+++ include/logger.h
@@ -12,7 +12,9 @@
 namespace moroxus {
 
     DECLARE_ENUM(LogLevel, NONE, ERROR, WARNING, INFO, DEBUG)
-
+    /**
+     * @brief The Logger class class for thread safe logging
+     */
     class Logger
     {
     private:
@@ -38,13 +40,27 @@ namespace moroxus {
             }
             return *this;
         }
-
+        /**
+         * @brief enableFile Enables logging into the file and sets name of that file.
+         * This method is not thread safe and influences all threads
+         * (All threads will start logging into that file).
+         * @param fileEnabled name of file for logging into
+         */
         static void enableFile(std::string fileEnabled = "log");
-
+        /**
+         * @brief disableFile Disables logging into file and influences all threads
+         * (All threads will stop logging into file).
+         */
         static void disableFile();
-
+        /**
+         * @brief enableConsole Enables logging to the console and influences all threads
+         * (All threads will start logging into console).
+         */
         static void enableConsole();
-
+        /**
+         * @brief disableConsole Disables logging to the console and influences all threads
+         * (All threads will start logging into console).
+         */
         static void disableConsole();
 
         ~Logger();
